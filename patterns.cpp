edf58933644892d00abdba0918a44390b4bbadd5
JAYANT RANJAN JHA <121686579+JAYANTRANJANJHA@users.noreply.github.com>
2025-07-16 14:44:46 +0000

    pattern 2

--- patterns.cpp
+++ patterns.cpp
@@ -52,6 +52,12 @@ int main()
 	
 	
 		// triangle patterns
+
+//  * 
+//  *  * 
+//  *  *  * 
+//  *  *  *  * 
+
 		for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < i + 1; j++){
@@ -60,6 +66,23 @@ int main()
 		}
 		cout<<endl;
 	}
+for (int i=0;i<n;i++){
+	for(int j=0;j<i+1;j++){
+		cout<<(i+1)<<"  ";
+		
+	}
+	cout<<endl;
+}
+char ch='A';
+for (int i=0;i<n;i++){
+	for(int j=0;j<i+1;j++){
+		cout<<ch<<"  ";
+	}
+	ch=ch+1;
+	cout<<endl;
+}
+
+
 
 	return 0;
 }
