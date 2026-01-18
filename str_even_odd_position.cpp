b3970b9cfae8d959d6a4b90a1a3512f1776420c5
JAYANT RANJAN JHA <121686579+JAYANTRANJANJHA@users.noreply.github.com>
2026-01-18 19:03:09 +0530

    simple string problem

--- str_even_odd_position.cpp
+++ str_even_odd_position.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_set>
 using namespace std;
 
 int main()
@@ -19,5 +20,38 @@ int main()
     }
   }
   cout << str_even + str_odd;
+
+  string str1 = "enjoy cold weather";
+  cout << " \ndo not " + str1 << endl;
+
+  string str_1 = "12345";
+  string str_2 = "10101";
+  string new_str = "";
+
+  for (int i = 0; i < str_1.length(); i++)
+  {
+    if (str_2[i] == '1')
+    {
+      new_str += str_1[i];
+    }
+  }
+
+  cout << new_str << endl;
+
+  string target = "communication";
+  string ref = "com";
+
+  unordered_set<char> refset(ref.begin(), ref.end());
+  string ans = "";
+  for (char ch : target)
+  {
+    if (refset.find(ch) == refset.end())
+    {
+      ans += ch;
+    }
+    
+  }
+  cout << ans<<endl;;
+
   return 0;
 }
